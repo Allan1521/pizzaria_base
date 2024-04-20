78bfe91e53378f651f7e38f25f3bf6942da2937d
Allan <allan1521lima@gmail.com>
2024-04-19 22:22:36 -0300

    pizzaria_base

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ pizzaJson.map((item, index) => {
 
     //clonando o pizza-item
     let pizzaItem = document.querySelector('.models .pizza-item').cloneNode(true);
-    
+
 
     // Definindo um atributo chamado 'data-key' ao elemento .pizza-item, contendo o id da pizza que foi selecionada pelo usuário.
     pizzaItem.setAttribute('data-key', index);
@@ -34,8 +34,7 @@ pizzaJson.map((item, index) => {
         document.querySelector('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
 
         document.querySelector('.pizzaInfo--size.selected').classList.remove('selected');
-document
-        .querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
+        document.querySelectorAll('.pizzaInfo--size').forEach((size, sizeIndex) => {
             // Fará que o tamanho grande sempre seja selecionada por padrão ao usuário clicar em uma pizza.
             if (sizeIndex == 2) {
                 size.classList.add('selected');
@@ -55,7 +54,7 @@ document
         //exibindo a janela modal na tela ao clicar em uma pizza
         document.querySelector('.pizzaWindowArea').style.display = 'flex';
 
-        setTimeout(() =>{
+        setTimeout(() => {
             document.querySelector('.pizzaWindowArea').style.opacity = 1;
         }, 200)
 
@@ -64,8 +63,40 @@ document
 
     });
 
+    //append = cria um elemento dentro do local especificado sempre na última posição, ou seja, não substitui os elementos que já estavam dentro.
+    document.querySelector('.pizza-area').append(pizzaItem);
+
 });
 
+//eventos do modal
+function closeModal() {
+    document.querySelector('.pizzaWindowArea').style.opacity = 0;
 
-//append = cria um elemento dentro do local especificado sempre na última posição, ou seja, não substitui os elementos que já estavam dentro.
-document.querySelector('.pizza-area').append(pizzaItem);
+    setTimeout(() => {
+        document.querySelector('.pizzaWindowArea').style.display = 'none';
+    }, 500)
+}
+
+
+/* Estamos usando forEach para que automaticamente ele selecione automaticamente cada um dos elementos com as classes abaixo e adicione um EventListener nesses elementos, a cada vez que o usuário clicar em um botão de fechar ele irá detectar o clique e irá chamar a função closeModal para fechar a janela. */
+document.querySelectorAll('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+});
+
+document.querySelector('.pizzaInfo--qtmais').addEventListener('click', () => {
+    //a cada vez que o usuário clicar no modal a variável modalQtd será incrementada em +1
+    modalQtd++;
+
+    //selecionando o elemento onde a quantidade de pizzas irá aparecer e atualizaando-o conforme a variável modalQtd
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQtd;
+});
+
+document.querySelector('.pizzaInfo--qtmenos').addEventListener('click', () => {
+
+    if (modalQtd >1){
+        modalQtd--;
+    }
+
+    document.querySelector('.pizzaInfo--qt').innerHTML = modalQtd;
+});
+//document.querySelector('.pizzaInfo--cancelButton').addEventListener('click',(event=>{}))
